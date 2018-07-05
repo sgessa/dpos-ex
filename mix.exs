d82ff4edda4f1743d016a016fbd1035b73907e49
Stefano Gessa <stefano@gessa.net>
2018-07-05 14:37:13 +0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Dpos.MixProject do
   use Mix.Project
 
   @desc "DPoS offline tools for Elixir"
-  @version "0.1.6"
+  @version "0.1.7"
   @url "https://github.com/lwfcoin/dpos-ex"
   @maintainers ["Stefano Gessa"]
 
